8730c731143d0d052dc30b2948b4cb84a690a86a
zyzyx159 <40427026+zyzyx159@users.noreply.github.com>
2026-09-14 04:11:39 -0500

    Switch package installation to xbps-install
    Replace apt package manager commands with xbps-install for compatibility.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # minimal base line apps for running ansible
-sudo apt -qq update && sudo apt -y install curl git gpg wget ansible
-
+#sudo apt -qq update && sudo apt -y install curl git gpg wget ansible
+sudo xbps-install -Syu && sudo xbps-install curl git gpg wget ansible
 # run ansible
 sudo ansible-pull -U https://github.com/zyzyx159/bootstrap.git -i "$(hostname --short),"
